4811969ba912d7f93a11a1442c00a727519f5257
rott3x <52593675+rott3x@users.noreply.github.com>
2019-07-07 19:44:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,4 +125,4 @@ if (message.content.startsWith(adminprefix + 'setava')) {
 });
 
 
-client.login(process.env.MBdlx1P4HmWGCmoOwFPiqWfWERtvv0Nx);
+client.login("NTk3NDY3MDc2NjU4OTg3MDE4.XSIg2w.XbB9re2jy4_L0x9NQv6EqISKJZE");
